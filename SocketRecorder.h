e484bd47e8b3545fe2d579c7ad48868ff3ecebe1
d3f4l7d <idaten.bekko@gmail.com>
2026-01-13 09:33:43 +0900

    Add files via upload

---
+++ SocketRecorder.h
@@ -0,0 +1,44 @@
+/// FicTrac http://rjdmoore.net/fictrac/
+/// \file       SocketRecorder.h
+/// \brief      Implementation of socket recorder based on boost::asio UDP datagrams.
+/// \author     Richard Moore
+/// \copyright  CC BY-NC-SA 3.0
+
+#pragma once
+
+#if 0	// TCP sockets
+
+#ifdef __APPLE__ || __linux__ 
+#include "SocketRecorder_linux.h"
+#elif _WIN32
+#include "SocketRecorder_win.h"
+#endif
+
+#else	// UDP sockets
+
+#include "RecorderInterface.h"
+
+#include <boost/asio.hpp>
+
+class SocketRecorder : public RecorderInterface
+{
+public:
+    SocketRecorder();
+    ~SocketRecorder();
+
+    /// Interface to be overridden by implementations.
+    bool openRecord(std::string host_port);
+    bool writeRecord(std::string s);
+    void closeRecord();
+
+private:
+    std::string _host;
+    int _port;
+    // Since io_service is deprecated (boost v1.66.0), switch to io_context
+    //boost::asio::io_service _io_service;
+    boost::asio::io_context _io_context;
+    boost::asio::ip::udp::socket _socket;
+    boost::asio::ip::udp::endpoint _endpoint;
+};
+
+#endif
