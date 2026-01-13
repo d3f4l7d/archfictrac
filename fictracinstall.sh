31a8af1d36e2cf36bbf287ecbc553f35d09c03ea
d3f4l7d <idaten.bekko@gmail.com>
2026-01-13 09:38:08 +0900

    Update fictracinstall.sh

--- fictracinstall.sh
+++ fictracinstall.sh
@@ -10,8 +10,12 @@ sudo pacman -Syu
 
 sudo pacman -S gcc cmake ffmpeg nlopt boost qt6 vtk hdf5 opencv
 git clone https://github.com/rjdmoore/fictrac.git ~/.config/fictrac
-wget -O 
 mkdir ~/.config/fictrac/build
 cd ~/.config/fictrac/build
 cmake ..
+wget -O ~/.config/fictrac/include/SocketRecorder.h https://github.com/d3f4l7d/archfictrac/SocketRecorder.h
+wget -O ~/.config/fictrac/src/SocketRecorder.cpp https://github.com/d3f4l7d/archfictrac/SocketRecorder.cpp
+wget -O ~/.config/fictrac/src/SerialRecorder.cpp https://github.com/d3f4l7d/archfictrac/SerialRecorder.cpp
 cmake --build . --config Release --parallel $(nproc) --clean-first
+
+echo "autoinstall of fictrac --DONE"
