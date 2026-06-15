5d0ec7df4afd844fb9eefd97730d3a37013dd220
d3f4l7d <idaten.bekko@gmail.com>
2026-06-15 11:29:48 +0000

    Create fictracsetmaker.sh

---
+++ fictracsetmaker.sh
@@ -0,0 +1,20 @@
+mkdir $HOME/front01
+mkdir $HOME/front02
+mkdir $HOME/front03
+mkdir $HOME/front04
+mkdir $HOME/front05
+mkdir $HOME/front06
+mkdir $HOME/front07
+mkdir $HOME/front08
+mkdir $HOME/front09
+mkdir $HOME/front10
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front01/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front02/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front03/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front04/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front05/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front06/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front07/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front08/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front09/
+cp $HOME/.config/fictrac/config/config_whitedot_q10.txt $HOME/front10/
